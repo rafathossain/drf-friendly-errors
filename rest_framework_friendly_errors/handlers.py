db38b2e81db9d16782d56d2aed8938a50d729e64
Franky Jose Quintero Rangel <frankyjquintero@gmail.com>
2019-09-04 19:36:07 -0500

    Update Handler: the response is set to the same standard as a response from a serializer

--- rest_framework_friendly_errors/handlers.py
+++ rest_framework_friendly_errors/handlers.py
@@ -1,8 +1,8 @@
 from rest_framework.views import exception_handler
 from rest_framework.exceptions import APIException
 
-from rest_framework_friendly_errors import settings
-from rest_framework_friendly_errors.utils import is_pretty
+from . import settings
+from .utils import is_pretty
 
 
 def friendly_exception_handler(exc, context):
@@ -15,13 +15,15 @@ def friendly_exception_handler(exc, context):
     if response is not None:
         if is_pretty(response):
             return response
-        error_message = response.data.get('detail', 'error')
-        error_code = settings.FRIENDLY_EXCEPTION_DICT.get(
-            exc.__class__.__name__)
-        response.data.pop('detail', {})
-        response.data['code'] = error_code
-        response.data['message'] = error_message
-        response.data['status_code'] = response.status_code
-        # response.data['exception'] = exc.__class__.__name__
+        error_message = response.data.get('detail', str(exc.__class__.__name__))
+        error_code = settings.FRIENDLY_EXCEPTION_DICT.get(exc.__class__.__name__)
+        data = response.data
+        errors = []
+        for field, value in data.items():
+            message = value[0] if type(value) is list else value
+            errors.append({"code": None, "field": field, "message": message})
+
+        response.data = {'code': error_code, 'message': error_message,
+                         'status_code': response.status_code, 'errors': errors}
 
     return response
